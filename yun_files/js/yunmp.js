d71497da80cf07b3a261508b634567fe55745b19
huangjun-best <huangjun_best@sina.com>
2013-12-19 15:19:55 +0800

    all

--- yun_files/js/yunmp.js
+++
@@ -1,4 +0,0 @@
-$('.nav_item>li').click(function(){
-	$(this).addClass('curr').siblings().removeClass('curr');	
-	$(this).children().find('a').add('curr').siblings().removeClass('curr');
-})
--- yunmingpian/yun_files/download.php
+++
@@ -1,7 +0,0 @@
-<?php
-$filename =$_REQUEST['filename'];
-header("Content-Type:text/plain");
-header('Content-Disposition:attachment;filename='.$filename);
-header('Content-Transfer-Encodeing: binary');
-readfile($filename);
-?>
